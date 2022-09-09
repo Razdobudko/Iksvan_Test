c6b76cc9f3d0c873c609ea990eaf34a260eec070
Razdabudzka <razdabudzka@icloud.com>
2022-09-09 10:27:11 +0300

    Initial Commit

---
+++ Iksvan_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Iksvan_Test
+//
+//  Created by Veranika Razdabudzka on 9.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
